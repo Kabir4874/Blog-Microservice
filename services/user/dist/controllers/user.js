326b264967aaba5940b0e4100a75b3880eaa460c
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-08-31 23:47:20 +0600

    user service

---
+++ services/user/dist/controllers/user.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+export const loginUser = async (req, res) => {
+    try {
+        const { email, name, image } = req.body;
+        let user = await User.findOne({ email });
+        if (!user) {
+            user = await User.create({
+                name,
+                email,
+                image,
+            });
+        }
+        const token = jwt.sign({ user }, process.env.JWT_SEC, {
+            expiresIn: "5d",
+        });
+        res.status(200).json({
+            message: "Login success",
+            token,
+            user,
+        });
+    }
+    catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+//# sourceMappingURL=user.js.map
