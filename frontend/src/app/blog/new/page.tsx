f247d71573ccb0ff751d1c717c3b039110bd4dc7
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-09-28 19:38:30 +0600

    update

--- frontend/src/app/blog/new/page.tsx
+++ frontend/src/app/blog/new/page.tsx
@@ -1,4 +1,26 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { RefreshCw } from "lucide-react";
+
+const blogCategories = [
+  "Technology",
+  "Health",
+  "Finance",
+  "Travel",
+  "Education",
+  "Entertainment",
+  "Study",
+];
 
 const AddBlog = () => {
   const handleSubmit = () => {};
@@ -9,7 +31,55 @@ const AddBlog = () => {
           <h2 className="text-2xl font-bold">Add New Blog</h2>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit}></form>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <Label>Title</Label>
+            <div className="flex justify-center items-center gap-2">
+              <Input name="title" required />
+              <Button type="button">
+                <RefreshCw />
+              </Button>
+            </div>
+            <Label>Description</Label>
+            <div className="flex justify-center items-center gap-2">
+              <Input name="description" required />
+              <Button type="button">
+                <RefreshCw />
+              </Button>
+            </div>
+
+            <Label>Category</Label>
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder={"Select category"} />
+              </SelectTrigger>
+              <SelectContent>
+                {blogCategories.map((e, i) => (
+                  <SelectItem key={i} value={e}>
+                    {e}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+
+            <div>
+              <Label>Image Upload</Label>
+              <Input type="file" accept="image/*" />
+            </div>
+
+            <div>
+              <Label>Blog Content</Label>
+              <div className="flex justify-between items-center mb-2">
+                <p className="text-sm text-muted-foreground">
+                  Paste you blog or type here. You can use rich text formatting.
+                  Please add image after improving your grammar.
+                </p>
+                <Button type="button" size={"sm"}>
+                  <RefreshCw size={16} />
+                  <span className="ml-2">Fix Grammar</span>
+                </Button>
+              </div>
+            </div>
+          </form>
         </CardContent>
       </Card>
     </div>
